16ddb0b29251885c1100bf2c13885b74bf62ba52
samuel <samuel2002espinoza@gmail.com>
2023-06-26 12:55:54 -0600

    terminando de hashear las passwords y el crear usuarios y guardarlos en la base de datos

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongose from 'mongoose'
 import { uniqueId } from '../utilis/index.js'
+import bcrypt from 'bcrypt'
 const userSchema = mongose.Schema({
     name: {
         type:String,
@@ -32,6 +33,17 @@ const userSchema = mongose.Schema({
     }
 })
 
+//este pre es un hook que nos permite hacer algo justo antes de guardar en la base de datos ya que se hayan pasado las validaciones
+userSchema.pre('save', async function (next){
+    //un password ya hasheado no lo volvera a hashear
+    if (!this.isModified('password')) {
+        next()
+    }
+    //numero de seguridad de hasheado
+    //esperamos que se genere el cifrado y luego esperramos que se junte con la contraseña para guardar
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+})
 const User = mongose.model('User', userSchema)
 
 export default User
