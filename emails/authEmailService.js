e7207ed5fd2914b3adcc6ffc80253a06094142e6
samuel <samuel2002espinoza@gmail.com>
2023-07-04 12:50:35 -0600

    terminamos con toda la verificacion de correos etc y comenzamos con jwt

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -2,21 +2,21 @@ import {createTransporter} from '../config/nodemailer.js'
 
 export async function sendEmailVerification({name, email, token}) {
     const transporter = createTransporter(
-        "sandbox.smtp.mailtrap.io",
-        2525,
-        "c30e6357b4c7b3",
-        "2f4a26e6644b1f"
+        process.env.EMAIL_HOST,
+        process.env.EMAIL_PORT,
+        process.env.EMAIL_USER,
+        process.env.EMAIL_PASS
     )
 
     //enviar el email
     const info = await transporter.sendMail({
-        from: 'AppSalon',
+        from: 'AppSalon <cuentas@appsalon.com>',
         to: email,
         subject: "AppSalon - Confirma tu cuenta",
         text: "AppSalon - Confirma tu cuenta",
         html: `<p>Hola: ${name}, confirma tu cuenta en AppSalon</p>
         <p>Tu cuenta esta casi lista, solo debes confirmarla en el siguiente enlace</p>
-        <a href='http://localhost:4000/api/auth/verify/${token}'>Confirmar cuenta</a>
+        <a href='${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}'>Confirmar cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
         `
     })
